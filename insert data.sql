0c15e959ede12bb6360a8b4ca6134c2a646a68a7
kratyam singh <karankushwaha7194@gmail.com>
2025-04-26 11:56:50 +0530

    Add files via upload

---
+++ insert data.sql
@@ -0,0 +1,9 @@
+use college;
+insert into student
+(s_id,s_name,s_age,s_course,s_rollno)
+values
+(1,'kapil',18,'bca',001),
+(2,'kratyam singh rathor',18,'bca',002),
+(3,'vikas',19,'bba',003),
+(4,'karan kumar',20,'b.com',004),
+(5,'kapil singh',19,'bca',005)
